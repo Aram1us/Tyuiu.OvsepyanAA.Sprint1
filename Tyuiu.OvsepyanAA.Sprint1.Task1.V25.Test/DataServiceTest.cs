f119d7edb00b9e7eb840729aedffd12afe2e3b02
aramovsepyan2006@outlook.com <aramovsepyan2006@outlook.com>
2024-10-16 17:40:08 +0500

    Task1

---
+++ Tyuiu.OvsepyanAA.Sprint1.Task1.V25.Test/DataServiceTest.cs
@@ -0,0 +1,23 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+
+using Tyuiu.OvsepyyanAA.Sprint1.Task1.V25.Lib;
+
+namespace Tyuiu.SpirinIS.Sprint1.Task1.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double x = 1.0;
+            double y = 2.0;
+            var res = ds.Calculate(x, y);
+            Assert.AreEqual(-1, res);
+
+        }
+    }
+}
